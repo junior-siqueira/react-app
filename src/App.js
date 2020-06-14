5ec2cdaf75a456c8aa3349795bbb91742738c408
JuniorGuita <junior.siqueira.ti@gmail.com>
2020-06-14 16:35:01 -0300

    feat: testando o envio de novos dados para deploy

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Projeto base para a aula de CI.
+          Testando o deploy automatizado.
         </p>
         <a
           className="App-link"
